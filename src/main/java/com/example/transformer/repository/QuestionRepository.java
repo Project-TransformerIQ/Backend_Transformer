57a5df8780c3fd5cf491904312cc1c699147b41e
Dulmin <dulmindulsara@gmail.com>
2025-08-26 18:28:53 +0530

    removed unwanted files

--- src/main/java/com/example/transformer/repository/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.transformer.repository;
-
-import com.example.transformer.model.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
